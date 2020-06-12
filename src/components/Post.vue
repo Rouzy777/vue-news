746e9b75077e2817d02c76f0c94381bf212bfdd6
Alexander <kharchenko7717@gmail.com>
2020-06-12 18:08:18 +0300

    Update Post.vue

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="">
-
+        <div class="card px-0 col-lg">
+            <img class="card-img-top" src="https://meduza.io/impro/z3TJgWXrC50GiTi6elMaVUJ0_l-aFF-kB_Wz-5y31mQ/fill/0/0/ce/1/aHR0cHM6Ly9tZWR1/emEuaW8vaW1hZ2Uv/YXR0YWNobWVudF9v/dmVycmlkZXMvaW1h/Z2VzLzAwNC84NTQv/NTU0L292L29IdUs1/cmlkRzBha3gyUkNr/V1NXVWcuanBn.webp" alt="Card image cap">
+            <div class="card-body pt-2">
+                <small class="text-uppercase font-weight-bold color">Новости</small>
+                <p class="card-text mt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis impedit facilis nesciunt dolorem fugiat assumenda iusto porro, ut excepturi molestiae?</p>
+                <p class="card-text"><small class="text-muted">3 часа назад</small></p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,5 +20,5 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
-    
+
 </style>
