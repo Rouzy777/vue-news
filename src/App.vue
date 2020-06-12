df9268b12f3a0cd6c16d5abc92385f7b384eb0b5
Alexander <kharchenko7717@gmail.com>
2020-06-12 18:03:40 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class='container' id="app">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="">
+                <a class="navbar-brand" href="#">News</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="ml-lg-3 collapse justify-content-center navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <router-link
+                        v-for='item in links'
+                        :to="item.to"
+                        :key='item.to'>
+                        {{item.name}}
+                    </router-link>
+                </div>
+            </div>
+            <div class="">
+                <span>EN</span>
+                <span class='mx-2'>|</span>
+                <span>RU</span>
+            </div>
+        </nav>
+        <router-view />
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            links: [
+                {
+                    to: '/',
+                    name: 'Home'
+                }
+            ]
+        })
+    }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <Post />
+        
+    </div>
+</template>
+
+<script>
+import Post from '@/components/Post.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        Post
+    }
+}
+</script>
